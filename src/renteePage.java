a5223f8b4d12de54a205ac3015b98b0ff5844192
BekLg <bereketlegesse22@gmail.com>
2023-01-13 16:17:44 +0300

    combo box items fetched from store database

--- src/renteePage.java
+++ src/renteePage.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.DriverManager;
+import java.util.Vector;
 
 public class renteePage extends JDialog {
     public JPanel renteePane;
     private JTextField tfName;
     private JTextField tfId;
-    private JTextField tfStoreNo;
     private JTextField tfPhoneNo;
     private JButton btnAddRentee;
     private JButton btnUpdate;
@@ -12,4 +15,27 @@ public class renteePage extends JDialog {
     private JButton btnRefresh;
     private JTable tblRentee;
     private JTextField tfContEndingDate;
+    private JComboBox cmbStoreNo;
+public renteePage() {
+
+    store st= new store();
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        st.connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1/buildingDB?user=root&password=root");
+        st.statement = st.connection.createStatement();
+        st.resultSet = st.statement.executeQuery("select storeNo from store where rented=false");
+        while (st.resultSet.next()) {
+            cmbStoreNo.addItem(st.resultSet.getObject(1));
+        }
+    }
+    catch (Exception ex) {
+        ex.printStackTrace();
+    }
+    btnAddRentee.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+           //
+        }
+    });
+}
 }
